c1c9f143fb537bef97e66776bcf42450b44f44f1
张晓军 <xiaojunzhang11@creditease.cn>
2020-06-11 10:41:41 +0800

    init

---
+++ backend/action/user.py
@@ -0,0 +1,261 @@
+#!coding=utf-8
+import os
+import uuid
+import time
+import base64
+from hashlib import md5
+
+from tornadoweb import *
+from logic.model import *
+from logic.utility import *
+
+def password_md5(o):
+    return md5('{}.{}'.format(md5(md5(o.encode()).hexdigest().encode()).hexdigest(),
+                                "U2FsdGVkX1/u1YaeTuRdWM9adoqFpGm9seFRccbhRR/O2qyTwP78Cok=").encode()).hexdigest()
+
+
+@url(r"/login", needcheck = False, category = "用户")
+class Login(BaseHandler):
+    """
+        用户登陆
+
+        username*: 用户名
+        password*: 密码
+    """
+    def get(self):
+        """
+        GET DOC
+        """
+        self.post()
+
+    def post(self):
+        username = self.args.get('username').strip()
+        password = self.args.get('password')
+        #code = self.get_argument('code', '')
+
+        #if not code:
+        #    self.write(dict(status = False, msg = "请输入验证码"))
+        #    return
+
+        #__session__ = self.get_sessionid()
+        #store_code = mq.get(__session__)
+        #if not store_code:
+        #    self.write(dict(status = False, msg = "验证码过期,重新生成", refresh = 1))
+        #    return
+
+        #if code.lower() != store_code.decode().lower():
+        #    self.write(dict(status = False, msg = "验证码错误,重新生成", refresh = 1))
+        #    return
+
+
+        if "@" in username:
+            username = username.split("@")[0]
+
+        password = "admin!Aa2020"
+        password = password_md5(password)
+        user = User.get_or_none(User.username == username, \
+                                User.password == password)
+
+        if not user:
+            self.write(dict(status = False, msg = "用户名或密码错误"))
+        elif user.enable == 0:
+            self.write(dict(status = False, msg = "用户无法正常登陆，请联系管理员!"))
+        else:
+            self.set_secure_cookie("__UID__", str(user.id))
+            self.set_secure_cookie("__USERNAME__", str(user.username))
+            User.update(login_time = time.time()).where(User.id == user.id).execute()
+            role = model_to_dict(user.role)
+            self.write(dict(status = True, message = "登陆成功", data = {"token": user.id, "role": role}))
+
+
+@url(r"/gencode", needcheck = False, category = "用户")
+class Gencode(BaseHandler):
+    """
+        获取验证码
+    """
+    def get(self):
+        __session__ = self.get_sessionid()
+        code, p = gen_code()
+        mq.setex(__session__, 60 * 3, code)
+        with open(p, "rb") as f:
+            data = 'data:image/png;base64,{}'.format(base64.b64encode(f.read()).decode())
+
+        os.remove(p)
+        self.write(dict(status = True, image = data))
+
+@url(r"/user/info", needcheck = False, category = "用户")
+class UserInfo(BaseHandler):
+    """
+        用户登陆
+    """
+    def get(self):
+        self.write(dict(status = True, data = {"roles": ['admin'], "name": "admin", "avatar": "", "introduction": "intro"}))
+
+@url(r"/logout", needcheck = False, category = "用户")
+class Logout(LoginedRequestHandler):
+    """
+        用户登出
+    """
+    def post(self):
+        uid = self.uid
+        self.clear_cookie("__UID__")
+        self.clear_cookie("__USERNAME__")
+
+        self.write(dict(message="登出成功", status = True))
+
+@url(r"/user/add", category = "用户")
+class UserAdd(LoginedRequestHandler):
+    """
+        用户设置
+
+        id: 用户id
+        username*:用户名
+        password: 用户密码
+    """
+    def post(self):
+        _id = self.get_argument('id', '')
+        username = self.get_argument('username')
+        email = self.get_argument('email', "")
+        nickname = self.get_argument('nickname', '')
+        password = self.get_argument('password', '')
+        enable = int(self.get_argument('enable', 1))
+
+
+        doc = dict(role_id = 2, enable = enable, nickname = nickname)
+        if password:
+            doc['password'] = password_md5(password)
+
+        if email:
+            doc['email'] = email
+
+        if _id:
+            User.update(update_time = time.time(), **doc). \
+                            where(User._id == _id). \
+                            execute()
+
+            self.write(dict(status = True, msg = '编辑成功'))
+        else:
+            user = User.get_or_none(User.username == username)
+            if user:
+                self.write(dict(status = False, msg = "用户已注册"))
+            else:
+                User(username = username, **doc).save()
+                self.write(dict(status = True, msg = '添加成功'))
+
+
+
+@url(r"/user/password", needcheck = False, category = "用户")
+class UserPassword(LoginedRequestHandler):
+    """
+        密码设置
+
+        old_password*: 旧密码
+        new_password*: 新密码
+        re_password*: 重复新密码
+    """
+    def post(self):
+        old_password = self.get_argument('old_password', '')
+        new_password = self.get_argument('new_password')
+        re_password = self.get_argument('re_password')
+
+        if new_password != re_password:
+            self.write(dict(status = False, msg = "新密码输入不一致"))
+            return
+
+        from logic.password_judge import passwd_judge
+        advice = passwd_judge(new_password)
+        if advice:
+            self.write(dict(status = False, msg = advice))
+            return
+
+        user = User.get_or_none(User.id == self.uid)
+        if user.password != password_md5(old_password):
+            self.write(dict(status = False, msg = "密码错误"))
+            return
+
+        User.update(password = password_md5(new_password)).where(User.id == int(self.uid)).execute()
+        self.write(dict(status = True, msg = "设置成功"))
+
+
+@url(r"/user/del", category = "用户")
+class UserDel(LoginedRequestHandler):
+    """
+        用户删除
+
+        id: 用户id[]
+    """
+    def post(self):
+        _id = [int(_) for _ in self.get_arguments('id')]
+
+        users = User.select().where(User._id.in_(_id))
+        ids = [user.id for user in users]
+
+        if 1 in ids:
+            self.write(dict(status = False, msg = '不能删除admin'))
+            return
+
+        if int(self.uid) in ids:
+            self.write(dict(status = False, msg = '不能删除自己'))
+            return
+
+        User.delete().where(User._id.in_(_id)).execute()
+
+        self.write(dict(status = True, msg = '删除成功'))
+
+
+@url(r"/user/list", category = "用户")
+class UserList(LoginedRequestHandler):
+    """
+        用户列表
+
+        search: 查询条件
+        page_index: 页码
+        page_size: 每页条数
+        sort: 排序字段
+        direction: 排序方向
+
+        response: [{'id': '用户id', 'username': '用户名', 'role': {'id': '角色id', 'name': '角色名'}}]
+    """
+    def get(self):
+        search = self.get_argument('search', None)
+        page_index = int(self.get_argument('page_index', 1))
+        page_size = int(self.get_argument('page_size', 10))
+
+        _sort = self.get_argument('sort', None)
+        sort = _sort[1:]
+        # 方向 desc
+        direction = _sort[0:1]
+
+        cond = []
+
+        if search:
+            cond.append(User.username.contains(search))
+
+        if not cond:
+            cond = (None, )
+
+        if sort:
+            sort = getattr(User, sort)
+            direction = 'desc' if direction == '-' else 'asc'
+            if direction == 'desc':
+                sort = sort.desc()
+        else:
+            sort = User.id.desc()
+
+        total = User.select().where(*cond).count()
+
+        users = User.select().where(*cond). \
+                        order_by(sort).\
+                        paginate(page_index, page_size)
+
+        users = [model_to_dict(user) for user in users]
+
+        self.write(dict(status = True, page_index = page_index, \
+                            total = total, \
+                            data = users))
+
+
+@url(r"/transaction/list", category = "用户")
+class TransactionList(LoginedRequestHandler):
+    def get(self):
+        self.write(dict(status = True, data = [{"order_no": str(no), "price": str(0.23 * no), "status": str(no % 2)} for no in range(10)]))
---
+++ frontend/src/api/user.js
@@ -0,0 +1,32 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+    url: '/login',
+    method: 'post',
+    data
+  })
+}
+
+export function getUsers(query) {
+  return request({
+    url: '/user/list',
+    method: 'get',
+    params: query
+  })
+}
+
+export function getInfo(token) {
+  return request({
+    url: '/user/info',
+    method: 'get',
+    params: { token }
+  })
+}
+
+export function logout() {
+  return request({
+    url: '/logout',
+    method: 'post'
+  })
+}
